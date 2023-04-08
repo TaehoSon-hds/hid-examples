96131dbe86d09fa46ef9bbf853d095015c1f50ca
Taeho Son <sth0622@haedosa.xyz>
2023-04-08 23:29:30 +0900

    Make all imports of vocab1.hs explicit

--- ch01/vocab1.hs
+++ ch01/vocab1.hs
@@ -1,14 +1,20 @@
-import Data.Char
+import Data.Char (isLetter)
 import Data.List (group, sort)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-import System.Environment
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
   [fname] <- getArgs
   text <- TIO.readFile fname
-  let ws = map head $ group $ sort $ map T.toCaseFold $ filter (not . T.null)
-           $ map (T.dropAround $ not . isLetter) $ T.words text
+  let ws =
+        map head $
+          group $
+            sort $
+              map T.toCaseFold $
+                filter (not . T.null) $
+                  map (T.dropAround $ not . isLetter) $
+                    T.words text
   TIO.putStrLn $ T.unwords ws
   print $ length ws
